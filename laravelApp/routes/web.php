9c32d5c616d36efcb6aebcdeab2952cbc6993883
Md Abu Hanife Nayem <028118@jb.com>
2024-02-07 14:41:53 +0600

    fresh

--- laravelApp/routes/web.php
+++ laravelApp/routes/web.php
@@ -14,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    echo "FFFFFFFFF";
-    $users = DB::select('select * from cim_suits');
-    dd($users);
     return view('welcome');
 });
